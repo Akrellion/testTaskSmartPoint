0d3f6e8e44aeec23f340425b640b73788f341877
v.e.strat <v.e.strat@neovoxtech.ru>
2024-03-19 14:35:34 +0300

    Мелкие правки стилей и вывода

--- resources/views/review/index.blade.php
+++ resources/views/review/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.main')
 @section('content')
     <div>
-        <a class="btn btn-primary mb-3" href="{{ route('review.update') }}">Обновить отзывы</a>
+        <a class="btn btn-primary mb-3 mt-3" href="{{ route('review.update') }}">Обновить отзывы</a>
     </div>
     <div>
         @if (session()->has('message'))
@@ -14,21 +14,21 @@
     <table class="table">
         <thead>
         <tr>
-            <th scope="col">Дата</th>
-            <th scope="col">Автор</th>
-            <th scope="col">Рейтинг</th>
+            <th class="text-lg-center" scope="col">Дата</th>
+            <th class="text-lg-center" scope="col">Автор</th>
+            <th class="text-lg-center" scope="col">Рейтинг</th>
             <th scope="col">Текст отзыва</th>
         </tr>
         </thead>
         <tbody>
         @foreach($reviews as $review)
             <tr>
-                <td>{{ $review->date_publication }}</td>
-                <td>{{ $review->author }}</td>
-                <td>{{ $review->rating }}</td>
+                <td>{{ \Carbon\Carbon::parse($review->date_publication)->format('d.m.Y')}}</td>
+                <td class="text-lg-center">{{ $review->author }}</td>
+                <td class="text-lg-center">{{ $review->rating }}</td>
                 <td>
-                    <div>
-                        {{ $review->text_title }}
+                    <div class="mb-2">
+                        <b>{{ $review->text_title }}</b>
                     </div>
                     <div>
                         {{ $review->text_like }}
